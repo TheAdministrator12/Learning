2fc949f048588cd6a117046da6dd6bfd3b2cf311
Adam Vaněk <97563545+TheAdministrator12@users.noreply.github.com>
2022-01-11 23:50:37 +0100

    Update test.lua

--- Lua/test.lua
+++ Lua/test.lua
@@ -1,3 +1,9 @@
 -- Taky tu nic není, sem budu jen dávat to co vytvořím.
+local i = 0
+local function Test()
+  print("Hello World!")
+end
 
-print("Test Hello World!")
+for i, 5, i += 1 do
+  Test()
+end
