f3fc5d64a6a71d1a9084a1fe9611334aec3c8db9
Adam Vaněk <97563545+TheAdministrator12@users.noreply.github.com>
2022-01-11 23:53:11 +0100

    Update test.lua

--- Lua/test.lua
+++ Lua/test.lua
@@ -1,9 +1,13 @@
 -- Taky tu nic není, sem budu jen dávat to co vytvořím.
 local i = 0
-local function Test()
-  print("Hello World!")
+local function Test() -- Tvoření funkce Test.
+  print("Hello World!") -- Kód který se spustí s funkcí.
 end
 
-for i, 5, i += 1 do
-  Test()
+for i, 5, i += 1 do -- For loop.
+  Test() -- Napíše to 6x "Hello World!" do konzole.
+end
+
+while i < 6 do -- While loop.
+  Test() -- Napíše to do konzole 5x "Hello World!"
 end
