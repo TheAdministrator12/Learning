bfebcf6dbf896d918c9fd47d14e47d4de41f2f7c
Adam Vaněk <97563545+TheAdministrator12@users.noreply.github.com>
2022-02-18 18:00:44 +0100

    Create Tags.lua

---
+++ Lua/Project#1 AoT/ServerScriptService/Tags.lua
@@ -0,0 +1,38 @@
+   --// MODULE SCRIPT\\--
+----// HOLDS ALL TAGS \\----
+
+local tags = {
+	
+  --[[
+  ["USER ID"] = { -- Player ID
+    ["TagText"] = "Name of the tag!"; -- Tag Name
+    ["TagColor"] = Color3.fromRGB(255, 255, 255) -- Takes RGB color value, determins the color of the tag!
+  }
+  
+  CHAT EXAMPLE:
+  [Contributor] [Guy]: Hello!
+  [OtherGuyWithNoTag]: No tag :(
+  ]]--
+  
+	["357829043"] = {
+		["TagText"] = "guy who made this game"; --Aka Me.
+		["TagColor"] = Color3.fromRGB(219, 129, 2)
+	};
+	
+	["178849946"] = {
+		["TagText"] = "Contributor"; -- Friend who helped me build some stuff :)
+		["TagColor"] = Color3.fromRGB(0, 143, 52)
+	};
+	
+	["213632057"] = {
+		["TagText"] = "Contributor"; -- Another friend helping me with big map :)
+		["TagColor"] = Color3.fromRGB(0, 143, 52)
+	};
+	["490730946"] = {
+		["TagText"] = "Moderator"; -- Epik game moderator!
+		["TagColor"] = Color3.fromRGB(8, 26, 42)
+	}
+	
+}
+
+return tags
