59e4be5a5f591b6e4be6fef265395e64adbca949
Adam Vaněk <97563545+TheAdministrator12@users.noreply.github.com>
2022-02-18 20:05:13 +0100

    Create DegToRadAndBack.c

---
+++ C/DegToRadAndBack.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+void ToDeg(float rad, float pi){
+    float math = rad * 180/pi;
+    printf("Uhel[Rad] %f | Uhel[Stup] %f\n", rad, math);
+}
+
+void ToRad(float stupne, float pi){
+    float math = stupne * pi/180;
+    printf("Uhel[Stup] %f | Uhel[Rad] %f\n", stupne, math);
+}
+
+void Tabulka(int pi){
+    printf("Sinus | Cosinus | Uhly [Radians] | Uhly [Stupne]\n");
+    for(int i = 0; i <= 360; i += 30){
+        float math = i * pi/180;
+        printf("%f | %f | %f | %d\n", sin(math), cos(math), math, i);
+    }
+}
+
+int WinMain(){
+    float pi = 3.14159265359;
+    ToDeg(2, pi);
+    ToRad(30, pi);
+    Tabulka(pi);
+    return 0;
+}
